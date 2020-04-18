46030aa381d6d5244b36c6930bfe874357370395
Tarmah <tiqbal.bscs16seecs@seecs.edu.pk>
2020-04-19 04:41:32 +0500

    Link Title to page of that specific title in Collection Preview Component.

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
@@ -8,20 +9,19 @@ import {
   PreviewContainer
 } from './collection-preview.styles';
 
-const CollectionPreview = ({ title, items }) => (
-
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
   <CollectionPreviewContainer>
-    <TitleContainer>{title.toUpperCase()}</TitleContainer>
+    <TitleContainer onClick={() => history.push(`${match.path}/${routeName}`)}>
+      {title.toUpperCase()}
+    </TitleContainer>
     <PreviewContainer>
       {items
         .filter((item, idx) => idx < 4)
-        .map(( item ) => (
+        .map(item => (
           <CollectionItem key={item.id} item={item} />
-      ))
-      }
+        ))}
     </PreviewContainer>
   </CollectionPreviewContainer>
-
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
