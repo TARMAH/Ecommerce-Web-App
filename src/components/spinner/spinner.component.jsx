a8f6e5178010b365f03c963e8da28ad42525b69b
Tarmah <tiqbal.bscs16seecs@seecs.edu.pk>
2020-05-25 05:42:06 +0500

    adding performance optimizations to codebase with react lazy, suspense and adding error checking feature in case there is any problem in the app

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { SpinnerContainer, SpinnerOverlay } from './spinner.styles';
+
+const Spinner = () => (
+
+  <SpinnerOverlay>
+    <SpinnerContainer />
+  </SpinnerOverlay>
+
+);
+
+export default Spinner;
--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
-import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+import Spinner from '../spinner/spinner.component';
 
-const WithSpinner = WrappedComponent => {
+const WithSpinner = WrappedComponent =>
 
-  const Spinner = ({ isLoading, ...otherProps }) => {
-    return isLoading ? (
-      <SpinnerOverlay>
-        <SpinnerContainer />
-      </SpinnerOverlay>
-    ) : (
-      <WrappedComponent {...otherProps} />
-    );
+({ isLoading, ...otherProps }) => {
+
+    return isLoading ? (<Spinner /> ) : (<WrappedComponent {...otherProps} /> );
+
   };
-  return Spinner;
-};
 
 export default WithSpinner;
